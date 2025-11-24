b48eef98540d88bf1cab9d73a30eb0236b1e2df1
w00fmeow <w00fmeow@protonmail.ch>
2025-11-24 16:57:53 +0200

    - update formula

--- pkg/brew/multi-query.rb
+++ pkg/brew/multi-query.rb
@@ -1,14 +1,14 @@
 class MultiQuery < Formula
-  version '0.0.6'
+  version '0.0.7'
   desc "Multi-database query executor with unified JSON output"
   homepage "https://github.com/w00fmeow/multi-query"
 
   if OS.mac?
       url "https://github.com/w00fmeow/multi-query/releases/download/#{version}/multi-query-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "e4a5232f0b5382eb0c3556f2637a8262b00ba4854fc451953e16a46eba83959c"
+      sha256 "0f886b5fdd56a55e3353c482b2e95e97ea8aabde6882f4aa7680ccb0c9a45844"
   elsif OS.linux?
       url "https://github.com/w00fmeow/multi-query/releases/download/#{version}/multi-query-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "aee0821bc3fcc4b6260658ba1526afb1a23f79ea606e5c2c596c9385749bf3db"
+      sha256 "b4e0ebbfe77fae7bca9ef830772d7d6d7556203f49ac89cdc60b4f44e6096000"
   end
 
   conflicts_with "multi-query"
